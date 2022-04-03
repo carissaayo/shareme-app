e4ab6b0c2a74a06fa2f9bc61b76b77922adc08ed
yussuf ajao <ajaoyussufayo@gmail.com>
2022-04-03 09:33:50 +0100

    redirecting non registered user to the login user

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import './App.css';
 import {Routes,Route,useNavigate} from "react-router-dom"
 import Login from './components/Login';
 import Home from './container/Home';
+import { useEffect } from 'react';
+import {fetchUser} from "./utils/fecthUser"
 function App() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const user = fetchUser()
+    if(!user) navigate("/login")
+  }, [])
+  
   return (
     <Routes>
       <Route path="login" element={<Login />} />
